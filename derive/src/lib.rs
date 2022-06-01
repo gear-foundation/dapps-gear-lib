30dd70cacce9e7652b1fb97d0e362223d85924b7
Shamil <shamil@shamil.ru>
2022-06-01 15:16:19 +0300

    Add module for non fungible token

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -6,6 +6,12 @@ extern crate proc_macro;
 #[allow(unused_imports)]
 use gear_lib_macros::*;
 
+// Multitoken
 declare_derive_storage_trait!(derive_mtk_state, StateKeeper, MTKStateKeeper);
 declare_impl_trait!(derive_mtk_token_state, MTKTokenState);
 declare_impl_trait!(derive_mtk_core, MTKCore);
+
+// NFT
+declare_derive_storage_trait!(derive_nft_state, NFTStateKeeper, NFTStateField);
+declare_impl_trait!(derive_nft_core, NFTCore);
+declare_impl_trait!(derive_nft_metastate, NFTMetaState);
