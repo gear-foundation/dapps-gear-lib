6b3ed29c2b9ba1cd0ecf02536ef28c1fa58e9404
Vasiliev Andrey <scrapland0@gmail.com>
2022-07-21 02:25:58 +0500

    add getting owner

--- src/non_fungible_token/nft_core.rs
+++ src/non_fungible_token/nft_core.rs
@@ -211,6 +211,15 @@ pub trait NFTCore: NFTStateKeeper {
         .expect("Error in reply `NFTApproval`, revoke_approval function");
     }
 
+    fn owner_of(&self, token_id: TokenId) {
+        let owner = *self
+            .get()
+            .owner_by_id
+            .get(&token_id)
+            .expect("NonFungibleToken: token does not exist");
+        msg::reply(owner.encode(), 0).expect("Error in reply 'ActorId', owner_of function");
+    }
+
     /// Returns a `Payout` struct for a given token
     /// If NFT contract has no royalties it just returns BtreeMap {“owner”: "amount"}
     fn nft_payout(&self, owner: &ActorId, amount: u128) -> Payout {
